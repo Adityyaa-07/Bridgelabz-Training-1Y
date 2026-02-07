05e136b3bbcb5091864d79a70f578b512fa8a55d
Aditya <aditya890346@gmail.com>
2026-02-07 18:47:30 +0530

    arrays added

---
+++ src/com/gla/arrays/LC1.java
@@ -0,0 +1,21 @@
+package com.gla.arrays;
+
+public class LC1 {
+    public int[] twoSum(int[] nums, int target) {
+        int[] ans = new int[2];
+        for(int i=0;i<nums.length;i++){
+            for(int j=i+1;j<nums.length;j++){
+                if(nums[i]+nums[j]==target){
+                    ans[0] =i;
+                    ans[i] =j;
+                    return ans;
+                }
+            }
+        }
+        return ans;
+    }
+public static void main(String[] args){
+
+}
+ }
+
